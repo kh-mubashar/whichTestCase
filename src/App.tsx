1bb0613089ff6c1b3ee4d3e430da2ac7b3b4d765
Mr. Zeus <mubasherkhawaja@gmail.com>
2022-12-07 11:06:22 +0000

    logic updates for selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,79 @@
 import "./App.css";
-import TabelCell from "./TableCell";
+// import TabelCell from "./TableCell";
+import React from "react";
 
 function TableGrid() {
+  const [matchValue, setMatchValue] = React.useState(0);
   const numberList = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
-    79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97,
-    98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112,
-    113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
-    128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142,
-    143, 144,
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+    [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
+    [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
+    [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+    [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72],
+    [73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84],
+    [85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96],
+    [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108],
+    [109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120],
+    [121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132],
+    [133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144],
   ];
-  const rows = numberList.map((row, i) => {
-    return (
-      <tr key={i}>
-        <TabelCell cellData={row} />
-      </tr>
-    );
-  });
+  const getMultipleOf = (col: number) => {
+    const multiples = [];
+    for (let i = col; i <= 144; i = i + col) {
+      multiples.push(i);
+    }
+    setMatchValue(col);
+  };
+  const checkIsMultiple = (value: number) => {
+    if (value % matchValue === 0) {
+      return true;
+    }
+    return false;
+  };
+  // const rows = numberList.map((row, i) => {
+  //   return (
+  //     <tr key={i}>
+  //       <TabelCell cellData={row} />
+  //     </tr>
+  //   );
+  // });
   return (
-    <table>
-      <tbody>{rows}</tbody>
-    </table>
+    // <table>
+    //   <tbody>{rows}</tbody>
+    // </table>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      {numberList.map((row, index) => (
+        <div key={index}>
+          {row.map((col, index) => (
+            <div
+              key={index}
+              className={String(col)}
+              style={{
+                height: 60,
+                width: 60,
+                border: "2px solid red",
+                margin: 5,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: checkIsMultiple(col) ? "#009FB7" : "#ECA400",
+              }}
+              onClick={() => getMultipleOf(col)}
+            >
+              {col}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
 
