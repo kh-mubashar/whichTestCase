bb33381ef84960a76e406f64a1d7333a8965c91e
Mr. Zeus <mubasherkhawaja@gmail.com>
2022-12-08 15:23:16 +0000

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,8 +2,7 @@ import TableGrid from "../TableGrid/TableGrid";
 import "./App.css";
 
 function App() {
-  const numberList = Array.from(Array(144).keys());
-
+  const numberList = Array.from({ length: 144 }, (_, i) => i + 1);
   return (
     <div data-testid='app-load' className='App'>
       <header className='App-header'>Which? Front-end Exercise?</header>
