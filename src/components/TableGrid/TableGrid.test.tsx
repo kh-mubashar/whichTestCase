634692f03b6cdf3660a8ad27a9584aaf18dccc92
Mr. Zeus <mubasherkhawaja@gmail.com>
2022-12-08 15:47:41 +0000

    Update TableGrid.test.tsx

--- src/components/TableGrid/TableGrid.test.tsx
+++ src/components/TableGrid/TableGrid.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import TableGrid from "./TableGrid";
 
-const numberList = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]];
+const numberList = Array.from({ length: 12 }, (_, i) => i + 1);
 
 test("renders empty grid table", () => {
   render(<TableGrid />);
@@ -9,19 +9,19 @@ test("renders empty grid table", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-// test("renders grid table Data", async () => {
-//   render(<TableGrid tableData={numberList} />);
-//   const linkElement = screen.getByTestId("table-grid");
-//   expect(linkElement).toBeInTheDocument();
-//   const DisplayVale = screen.getByText(/5/i);
-//   expect(DisplayVale).toBeInTheDocument();
-// });
+test("renders grid table Data", async () => {
+  render(<TableGrid tableData={numberList} />);
+  const linkElement = screen.getByTestId("table-grid");
+  expect(linkElement).toBeInTheDocument();
+  const DisplayVale = screen.getByText(/5/i);
+  expect(DisplayVale).toBeInTheDocument();
+});
 
-// test("highlight the multiple of selection", async () => {
-//   render(<TableGrid tableData={numberList} />);
-//   const eventTrigger = screen.getByTestId("trigger6");
-//   const expectedTrigger = screen.getByTestId("trigger12");
-//   fireEvent.click(eventTrigger);
-//   expect(eventTrigger).toHaveStyle("background-color: rgb(0, 159, 183)");
-//   expect(expectedTrigger).toHaveStyle("background-color: rgb(0, 159, 183)");
-// });
+test("highlight the multiple of selection", async () => {
+  render(<TableGrid tableData={numberList} />);
+  const eventTrigger = screen.getByTestId("trigger6");
+  const expectedTrigger = screen.getByTestId("trigger12");
+  fireEvent.click(eventTrigger);
+  expect(eventTrigger).toHaveStyle("background-color: rgb(0, 159, 183)");
+  expect(expectedTrigger).toHaveStyle("background-color: rgb(0, 159, 183)");
+});
